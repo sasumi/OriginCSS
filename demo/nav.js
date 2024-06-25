03f8b5ac7cca787a9bdf69e967594c46f7774af0
sasumi <cnsasumi@gmail.com>
2024-06-25 18:59:53 +0800

    upd

--- demo/nav.js
+++ demo/nav.js
@@ -14,13 +14,14 @@ document.write(`
     </nav>  
 </header>
 <style>
-    body {padding:1em;}
+    body {padding:0 1em;}
     h1, h2, h3, h4, h5, h6 {margin:0.5em 0;}
     h1 {font-size:2rem;}
     h2 {font-size:1.6rem;}
     h3 {font-size:1.4rem;}
     h4 {font-size:1.2rem;}
     h5 {font-size:1rem;}
+    header {margin-bottom:1em; border-bottom:1px solid #ccc; padding-bottom:1em;}
     .main-nav {display:flex; gap:1em;}
     .main-nav a {display:block; padding:0.5em 2em; border:1px solid var(--color-link); border-radius:20px}
     .main-nav a.active {background-color:var(--color-link); color:white;}
